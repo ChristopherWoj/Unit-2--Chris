20b74109d7a3a84509eaf3728e04e34cbdbdc26c
ChristopherWoj <christopherw334@nycstudents.net>
2026-09-25 10:13:18 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+bill = 50
+service = okay
+
+if service == okay:
+    print (bill * 15%)
+    print ("The service was okay. Heres a 15% tip")
+if service = bad:
+    print (bill * 0%)
+    print ("The service is terrible! No tip for you")
+if service = good:
+    print (bill * 20%)
+    print ("The service was pretty good. Heres a 20% tip")
+if service = great:
+    print (bill * 25%)
+    print ("The service was great! Heres a 25% tip")
